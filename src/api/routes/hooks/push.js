b3ff7a403e3a7a0184a5d273c71453af5d2f5b6f
Gabriel Frattini <gabbefrattini@gmail.com>
2023-05-07 16:14:50 +0200

    add some logs

--- src/api/routes/hooks/push.js
+++ src/api/routes/hooks/push.js
@@ -1,34 +1,51 @@
 export default async (req, res) => {
-  const { klarna_order_id } = req.query
+  const { klarna_order_id } = req.query;
 
   function isPaymentCollection(id) {
-    return id && id.startsWith("paycol")
+    return id && id.startsWith("paycol");
   }
 
   try {
-    const orderService = req.scope.resolve("orderService")
-    const klarnaProviderService = req.scope.resolve("pp_klarna")
+    const orderService = req.scope.resolve("orderService");
+    const klarnaProviderService = req.scope.resolve("pp_klarna");
 
-    const klarnaOrder = await klarnaProviderService.retrieveCompletedOrder(
-      klarna_order_id
-    )
+    console.log("/klarna/push: klarna_order_id: ", klarna_order_id);
 
-    const resourceId = klarnaOrder.merchant_data
+    try {
+      const klarnaOrder = await klarnaProviderService.retrieveCompletedOrder(
+        klarna_order_id
+      );
+      console.log("/klarna/push: klarnaOrder: ", klarnaOrder);
+    } catch (error) {
+      console.log("/klarna/push: error: ", error);
+      klarnaOrder = {
+        order_id: klarna_order_id,
+      };
+    }
 
+    const resourceId = klarnaOrder.merchant_data;
+
+    console.log(
+      "/klarna/push: isPaymentCollection: ",
+      isPaymentCollection(resourceId)
+    );
     if (isPaymentCollection(resourceId)) {
-      await klarnaProviderService.acknowledgeOrder(klarnaOrder.order_id)
+      await klarnaProviderService.acknowledgeOrder(klarnaOrder.order_id);
+      console.log("/klarna/push: acknowledgeOrder: ", klarnaOrder.order_id);
     } else {
-      const order = await orderService.retrieveByCartId(resourceId)
+      const order = await orderService.retrieveByCartId(resourceId);
+      console.log("/klarna/push: order: ", order);
 
       await klarnaProviderService.acknowledgeOrder(
         klarnaOrder.order_id,
         order.id
-      )
+      );
+      console.log("/klarna/push: acknowledgeOrder 2: ", klarnaOrder.order_id);
     }
 
-    res.sendStatus(200)
+    res.sendStatus(200);
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
-}
+};
